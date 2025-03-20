c6d13f85a84ef4511b685bab74aa3b41a0f9a15c
amit kumar <107244837+amitsingh2775@users.noreply.github.com>
2025-03-20 22:24:05 +0530

    Update socket.js

--- src/socket.js
+++ src/socket.js
@@ -7,6 +7,7 @@ sessionStorage.setItem("userId", userID);
 const socket = io("https://connect-match-backend.vercel.app/", {
   transports: ["polling"],
   query: { userID },
+  withCredentials: true,
 });
 
 export default socket;
