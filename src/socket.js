701d0238f16e719f612c7b36cf5ccb103d2d2d43
amit kumar <107244837+amitsingh2775@users.noreply.github.com>
2025-03-20 18:37:08 +0530

    Update socket.js

--- src/socket.js
+++ src/socket.js
@@ -4,7 +4,7 @@ import io from "socket.io-client";
 const userID = sessionStorage.getItem("userId") || `user_${Math.random().toString(36).slice(2, 11)}`;
 sessionStorage.setItem("userId", userID);
 
-const socket = io("http://localhost:8000", {
+const socket = io("https://connect-match-backend.vercel.app/", {
   transports: ["websocket"],
   query: { userID },
 });
