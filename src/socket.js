dff633fa53bfe0d9de23871219bab212b1c0142f
amit kumar <107244837+amitsingh2775@users.noreply.github.com>
2025-03-21 18:43:19 +0530

    Update socket.js

--- src/socket.js
+++ src/socket.js
@@ -3,7 +3,7 @@ import io from "socket.io-client";
 const userID = sessionStorage.getItem("userId") || `user_${Math.random().toString(36).slice(2, 11)}`;
 sessionStorage.setItem("userId", userID);
 
-const socket = io("https://connect-match-backend.vercel.app", {
+const socket = io("https://connectmatch-backend.onrender.com", {
   query: { userID },
   withCredentials: true,
 });
